92e4202be1df2fc7a26a393f29eff7d5818b9d72
fabian <fabian.gallardo@unc.edu.ar>
2021-04-18 16:55:13 -0300

    terminado

--- models/Producto.php
+++ models/Producto.php
@@ -1,100 +1,66 @@
 <?php
-    //! Producto Hereda la información de Conectar !//
     class Producto extends Conectar{
-        //¿ Traer todos los datos ¿//
         public function get_producto(){
-            //# conexion #//
-            $conectar = parent::conexion();
+            $conectar= parent::conexion();
             parent::set_names();
-            $sql="SELECT* FROM tm_product  where est=1";
+            $sql="SELECT * FROM tm_producto WHERE est=1";
             $sql=$conectar->prepare($sql);
             $sql->execute();
-
-            return $resultado = $sql->fetchAll();
-
+            return $resultado=$sql->fetchAll();
         }
 
-        
-        //¿ Traer los datos del id que coincida ¿//
-        public function get_producto_x_id($pro_id){
-            //# conexion #//
-            $conectar = parent::conexion();
+        public function get_producto_x_id($prod_id){
+            $conectar= parent::conexion();
             parent::set_names();
-            $sql="SELECT* FROM tm_product where prod_id = ?";
+            $sql="SELECT * FROM tm_producto WHERE prod_id = ?";
             $sql=$conectar->prepare($sql);
-            $sql->bindValue(1,$pro_id);
+            $sql->bindValue(1,$prod_id);
             $sql->execute();
-
-            return $resultado = $sql->fetchAll();
-
+            return $resultado=$sql->fetchAll();
         }
 
-        
-        //¿ Elimina los datos del id que coincida ¿//
-        public function delete_producto($pro_id){
-            //# conexion #//
-            $conectar = parent::conexion();
+        public function delete_producto($prod_id){
+            $conectar= parent::conexion();
             parent::set_names();
             $sql="UPDATE tm_producto
-                    SET
-                        est =0,
-                        fech_elim=now()
-                    WHERE
-                        prod_id = ?";
+                SET
+                    est=0,
+                    fech_elim=now()
+                WHERE
+                    prod_id = ?";
             $sql=$conectar->prepare($sql);
-            $sql->bindValue(1,$pro_id);
+            $sql->bindValue(1,$prod_id);
             $sql->execute();
-
-            return $resultado = $sql->fetchAll();
-
+            return $resultado=$sql->fetchAll();
         }
 
-        
-
-        
-        //¿ Insertar los datos  ¿//
-        public function insert_producto($pro_nom){
-            //# conexion #//
-            $conectar = parent::conexion();
+        public function insert_producto($prod_nom){
+            $conectar= parent::conexion();
             parent::set_names();
-            $sql="INSERT INTO tm_producto
-                        (prod_id,prod_nom,fech_crea,fech_modi,fech_elim,est)
-                    VALUES
-                        (NULL, ?, now(), NULL, NULL, 1);
-                   ";
+            $sql="INSERT INTO tm_producto (prod_id, prod_nom, fech_crea, fech_modi, fech_elim, est) VALUES (NULL, ?, now(), NULL, NULL, 1);";
             $sql=$conectar->prepare($sql);
-            $sql->bindValue(1,$pro_nom);
+            $sql->bindValue(1,$prod_nom);
             $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
 
-            return $resultado = $sql->fetchAll();
-
-        }     
-        
-        
-        //¿ actualizar los datos del id que coincida ¿//
-        public function update_producto($pro_id,$pro_nom){
-            //# conexion #//
-            $conectar = parent::conexion();
+        public function update_producto($prod_id,$prod_nom){
+            $conectar= parent::conexion();
             parent::set_names();
             $sql="UPDATE tm_producto
-                    SET
-                        pro_nom =?,
-                        fech_modi=now()
-                    WHERE
-                        prod_id = ?";
+                SET
+                    prod_nom=?,
+                    fech_modi=now()
+                WHERE
+                    prod_id = ?";
             $sql=$conectar->prepare($sql);
-            $sql->bindValue(1,$pro_nom);
-            $sql->bindValue(2,$pro_id);
+            $sql->bindValue(1,$prod_nom);
+            $sql->bindValue(2,$prod_id);
             $sql->execute();
-
-            return $resultado = $sql->fetchAll();
-
+            return $resultado=$sql->fetchAll();
         }
+
+
+
     }
-
-
-
-        
-
-
 ?>
--- view/MntProducto/index.php
+++ view/MntProducto/index.php
@@ -28,13 +28,16 @@
     <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
     <meta name="author" content="ThemePixels">
 
-    <title>Sistema de producto php mysql y ajax</title>
+    <title>Mantenimiento de Producto</title>
 
     <!-- vendor css -->
     <link href="../../public/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
     <link href="../../public/lib/Ionicons/css/ionicons.css" rel="stylesheet">
     <link href="../../public/lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
     <link href="../../public/lib/jquery-switchbutton/jquery.switchButton.css" rel="stylesheet">
+
+    <link href="../../public/lib/datatables/jquery.dataTables.css" rel="stylesheet">
+    <link href="../../public/datatables/buttons.dataTables.min.css" rel="stylesheet"/>
 
     <!-- Bracket CSS -->
     <link rel="stylesheet" href="../../public/css/bracket.css">
@@ -43,31 +46,65 @@
   <body>
 
     <!-- ########## START: LEFT PANEL ########## -->
-    <div class="br-logo"><a href=""><span>[</span>Crud<span>]</span></a></div>
+    <div class="br-logo"><a href=""><span>[</span>CRUD<span>]</span></a></div>
     <div class="br-sideleft overflow-y-auto">
       <label class="sidebar-label pd-x-15 mg-t-20">Navigation</label>
       <div class="br-sideleft-menu">
-        <a href="../Home/index.php" class="br-menu-link">
+        <a href="../Home/" class="br-menu-link">
           <div class="br-menu-item">
             <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
             <span class="menu-item-label">Dashboard</span>
           </div><!-- menu-item -->
         </a><!-- br-menu-link -->
-        
-       
+
         <a href="#" class="br-menu-link">
           <div class="br-menu-item">
             <i class="menu-item-icon icon ion-ios-filing-outline tx-24"></i>
             <span class="menu-item-label">Mantenimiento</span>
             <i class="menu-item-arrow fa fa-angle-down"></i>
-          </div><!-- menu-item -->
-        </a><!-- br-menu-link -->
-
+          </div>
+        </a>
         <ul class="br-menu-sub nav flex-column">
-          <li class="nav-item"><a href="accordion.html" class="nav-link">Producto</a></li>
-          
+          <li class="nav-item"><a href="../MntProducto/" class="nav-link">Producto</a></li>
         </ul>
-      </div><!-- br-sideleft-menu -->
+      </div>
+
+      <label class="sidebar-label pd-x-15 mg-t-25 mg-b-20 tx-info op-9">Information Summary</label>
+
+      <div class="info-list">
+        <div class="d-flex align-items-center justify-content-between pd-x-15">
+          <div>
+            <p class="tx-10 tx-roboto tx-uppercase tx-spacing-1 tx-white op-3 mg-b-2 space-nowrap">Memory Usage</p>
+            <h5 class="tx-lato tx-white tx-normal mg-b-0">32.3%</h5>
+          </div>
+          <span class="peity-bar" data-peity='{ "fill": ["#336490"], "height": 35, "width": 60 }'>8,6,5,9,8,4,9,3,5,9</span>
+        </div><!-- d-flex -->
+
+        <div class="d-flex align-items-center justify-content-between pd-x-15 mg-t-20">
+          <div>
+            <p class="tx-10 tx-roboto tx-uppercase tx-spacing-1 tx-white op-3 mg-b-2 space-nowrap">CPU Usage</p>
+            <h5 class="tx-lato tx-white tx-normal mg-b-0">140.05</h5>
+          </div>
+          <span class="peity-bar" data-peity='{ "fill": ["#1C7973"], "height": 35, "width": 60 }'>4,3,5,7,12,10,4,5,11,7</span>
+        </div><!-- d-flex -->
+
+        <div class="d-flex align-items-center justify-content-between pd-x-15 mg-t-20">
+          <div>
+            <p class="tx-10 tx-roboto tx-uppercase tx-spacing-1 tx-white op-3 mg-b-2 space-nowrap">Disk Usage</p>
+            <h5 class="tx-lato tx-white tx-normal mg-b-0">82.02%</h5>
+          </div>
+          <span class="peity-bar" data-peity='{ "fill": ["#8E4246"], "height": 35, "width": 60 }'>1,2,1,3,2,10,4,12,7</span>
+        </div><!-- d-flex -->
+
+        <div class="d-flex align-items-center justify-content-between pd-x-15 mg-t-20">
+          <div>
+            <p class="tx-10 tx-roboto tx-uppercase tx-spacing-1 tx-white op-3 mg-b-2 space-nowrap">Daily Traffic</p>
+            <h5 class="tx-lato tx-white tx-normal mg-b-0">62,201</h5>
+          </div>
+          <span class="peity-bar" data-peity='{ "fill": ["#9C7846"], "height": 35, "width": 60 }'>3,12,7,9,2,3,4,5,2</span>
+        </div><!-- d-flex -->
+      </div><!-- info-lst -->
+
       <br>
     </div><!-- br-sideleft -->
     <!-- ########## END: LEFT PANEL ########## -->
@@ -643,23 +680,41 @@
     <div class="br-mainpanel">
       <div class="br-pageheader pd-y-15 pd-l-20">
         <nav class="breadcrumb pd-0 mg-0 tx-12">
-          <a class="breadcrumb-item" href="index.html">Bracket</a>
-          <span class="breadcrumb-item active">Blank Page</span>
+          <a class="breadcrumb-item" href="index.html">Mantenimiento</a>
+          <span class="breadcrumb-item active">Producto</span>
         </nav>
       </div><!-- br-pageheader -->
       <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
-        <h4 class="tx-gray-800 mg-b-5">Blank Page (Default Layout)</h4>
-        <p class="mg-b-0">Introducing Bracket admin template, the most handsome admin template of all time.</p>
+        <h4 class="tx-gray-800 mg-b-5">Producto</h4>
+        <p class="mg-b-0">Desde esta ventana podra dar mantenimiento a productos</p>
       </div>
 
       <div class="br-pagebody">
+        <div class="br-section-wrapper">
+            <h6 class="tx-gray-800 tx-uppercase tx-bold tx-14 mg-b-10">Mantenimiento de Producto</h6>
+            <button id="btnnuevo" class="btn btn-outline-primary btn-block mg-b-10">Nuevo Registro</button>
 
-        <!-- start you own content here -->
+            <div class="table-wrapper">
+              <table id="producto_data" class="table display responsive nowrap">
+                <thead>
+                  <tr>
+                    <th class="wd-15p">Nombre</th>
+                    <th class="wd-15p"></th>
+                    <th class="wd-20p"></th>
+                  </tr>
+                </thead>
+                <tbody>
 
-      </div><!-- br-pagebody -->
+                </tbody>
+              </table>
+            </div>
+        </div>
+      </div>
 
     </div><!-- br-mainpanel -->
     <!-- ########## END: MAIN PANEL ########## -->
+
+    <?php require_once("modalmantenimiento.php");?>
 
     <script src="../../public/lib/jquery/jquery.js"></script>
     <script src="../../public/lib/popper.js/popper.js"></script>
@@ -669,7 +724,18 @@
     <script src="../../public/lib/jquery-ui/jquery-ui.js"></script>
     <script src="../../public/lib/jquery-switchbutton/jquery.switchButton.js"></script>
     <script src="../../public/lib/peity/jquery.peity.js"></script>
-
     <script src="../../public/js/bracket.js"></script>
+
+    <script src="../../public/lib/datatables/jquery.dataTables.js"></script>
+    <script src="../../public/lib/datatables-responsive/dataTables.responsive.js"></script>
+
+    <script src="../../public/datatables/dataTables.buttons.min.js"></script>
+    <script src="../../public/datatables/buttons.html5.min.js"></script>
+    <script src="../../public/datatables/buttons.colVis.min.js"></script>
+    <script src="../../public/datatables/jszip.min.js"></script>
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+    <script type="text/javascript" src="mntproducto.js"></script>
   </body>
 </html>
